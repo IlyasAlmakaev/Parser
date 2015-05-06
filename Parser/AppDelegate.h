20a290874a24d0afad35015178d4e67f05036cb5
IlyasAlmakaev <i.almakaev@gmail.com>
2015-05-06 12:11:31 +0700

    Initial Commit

---
+++ Parser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Parser
+//
+//  Created by Admin on 06.05.15.
+//  Copyright (c) 2015 intent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Parser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Parser
+//
+//  Created by Admin on 06.05.15.
+//  Copyright (c) 2015 intent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
